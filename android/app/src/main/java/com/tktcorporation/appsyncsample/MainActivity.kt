d8aade0eb1bf19224efd77d017879297451ed15f
tkt <tktcorporation.go@gmail.com>
2020-12-11 15:46:27 +0900

    fail to setup of appsync

--- android/app/src/main/java/com/tktcorporation/appsyncsample/MainActivity.kt
+++ android/app/src/main/java/com/tktcorporation/appsyncsample/MainActivity.kt
@@ -2,10 +2,23 @@ package com.tktcorporation.appsyncsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.amazonaws.mobile.config.AWSConfiguration
+import com.amplifyframework.AmplifyException
+import com.amplifyframework.core.Amplify
+import com.amplifyframework.datastore.AWSDataStorePlugin
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+
+    try {
+      Amplify.addPlugin(AWSDataStorePlugin())
+      Amplify.configure(applicationContext)
+      Log.i("Tutorial", "Initialized Amplify")
+    } catch (failure: AmplifyException) {
+      Log.e("Tutorial", "Could not initialize Amplify", failure)
     }
+  }
 }
