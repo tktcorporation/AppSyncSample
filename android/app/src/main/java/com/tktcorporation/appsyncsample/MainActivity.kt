c84deff57ab15716a20370743b5e113160fe5ca8
tkt <tktcorporation.go@gmail.com>
2020-12-11 16:47:22 +0900

    success to initialize amplify

--- android/app/src/main/java/com/tktcorporation/appsyncsample/MainActivity.kt
+++ android/app/src/main/java/com/tktcorporation/appsyncsample/MainActivity.kt
@@ -2,23 +2,10 @@ package com.tktcorporation.appsyncsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.amazonaws.mobile.config.AWSConfiguration
-import com.amplifyframework.AmplifyException
-import com.amplifyframework.core.Amplify
-import com.amplifyframework.datastore.AWSDataStorePlugin
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
-
-    try {
-      Amplify.addPlugin(AWSDataStorePlugin())
-      Amplify.configure(applicationContext)
-      Log.i("Tutorial", "Initialized Amplify")
-    } catch (failure: AmplifyException) {
-      Log.e("Tutorial", "Could not initialize Amplify", failure)
-    }
   }
 }
---
+++ android/app/src/main/java/com/tktcorporation/appsyncsample/MyAmplifyApp.kt
@@ -0,0 +1,19 @@
+package com.tktcorporation.appsyncsample
+
+import android.app.Application
+import android.util.Log
+import com.amplifyframework.AmplifyException
+import com.amplifyframework.core.Amplify
+
+class MyAmplifyApp: Application() {
+  override fun onCreate() {
+    super.onCreate()
+
+    try {
+      Amplify.configure(applicationContext)
+      Log.i("MyAmplifyApp", "Initialized Amplify")
+    } catch (error: AmplifyException) {
+      Log.e("MyAmplifyApp", "Could not initialize Amplify", error)
+    }
+  }
+}
